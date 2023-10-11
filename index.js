ae2be58abde1861d55f740cdb0c4b127a95a11bb
FilipStaigl <147478726+FilipStaigl@users.noreply.github.com>
2023-10-11 09:36:23 +0200

    aaaa

---
+++ index.js
@@ -0,0 +1,15 @@
+const article = document.querySelector("#content");
+ const button = document.querySelector("#read-more");
+
+button.addEventListener("click", readMore);
+
+function readMore() {
+     if (article.className == "open") {
+       // Read less
+     article.className = "";
+     button.innerHTML = "Show more";
+   } else {
+     article.className = "open";
+     button.innerHTML = "Show less";
+   }
+}
